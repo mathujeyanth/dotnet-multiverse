81a9f7cdc02bb3074a282e8b2941eacc44475c9c
Mathu Jeyanth Mahendrarajh <maje@mir-robots.com>
2025-01-22 14:41:15 +0100

    With correct extension

--- DotnetMultiverse/DotnetMultiverse/DotnetMultiverse/Startup/AudioFormats/Mp3Audio.cs
+++ DotnetMultiverse/DotnetMultiverse/DotnetMultiverse/Startup/AudioFormats/Mp3Audio.cs
@@ -5,7 +5,7 @@ public record Mp3Audio : IAudio
     public required Stream AudioStream { get; init; }
     public required TimeSpan Duration { get; init; }
     public required int SampleRate { get; init; }
-    
+    public string Extension => "mp3";
     public void Dispose()
     {
         AudioStream.Dispose();
