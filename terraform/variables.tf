0fda4abeee7507d6a9c34d00509fec021d20e7b3
Jeyanth <mathujeyanth@hotmail.dk>
2025-08-02 16:12:13 +0200

    Trigger docker image update on image change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,9 @@ variable "docker_image" {
   description = "Docker image with digest"
   type        = string
 }
+
+variable "ec2_private_key" {
+  description = "Private key for EC2 SSH access"
+  type        = string
+  sensitive   = true
+}
